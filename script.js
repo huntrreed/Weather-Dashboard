231ae23e400d76cb70201988420ee1daf9fb2a02
huntrreed <146265520+huntrreed@users.noreply.github.com>
2023-11-26 13:40:21 -0700

    added some bootstrap for layout/design

---
+++ script.js
@@ -0,0 +1,23 @@
+var 
+var
+var getWeather = function (CityName) {
+    var apiURL = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key};
+
+    fetch(apiURL)
+    .then(function(response){
+        if (response.ok){
+            response.json().then(function (data)) {
+                displayWeather(data,cityName);
+            });
+            } else {
+            alert('Error:' + response.StatusText)
+            }
+
+        .catch(function (error){
+            alert('Unable to get weather data');
+        });
+    }
+   
+    import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
