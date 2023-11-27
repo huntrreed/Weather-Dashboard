d9b69b3b1f3ae12dddc21f4aef2fd822fff56f7c
huntrreed <146265520+huntrreed@users.noreply.github.com>
2023-11-27 10:13:06 -0700

    current weather card updating

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ document.getElementById('search-form').addEventListener('submit', function(event
     var city = document.getElementById('city-input').value;
     getGeoLocation(city);
 });
+/*saving seatched cities*/
+function saveSearchHistory(cityName) {
+    let history = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    if (!history.includes(cityName)) {
+        history.push(cityName);
+        localStorage.setItem('searchHistory', JSON.stringify(history));
+    }
+}
 
 //*converting the location into lat and lon so it can work with the city location
 function getGeoLocation(cityName) {
@@ -25,11 +33,12 @@ function getGeoLocation(cityName) {
     .catch(error => {
         console.error('Error fetching location:', error);
     });
+    saveSearchHistory(cityName);
 }
 //*getting weather data for the lat and lon of the city
 function getWeather(latitude, longitude) {
     var apiKey = '91ce4240cdd8015ed3ef68dc83c67b37'
-    var weatherapiURL = 'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${91ce4240cdd8015ed3ef68dc83c67b37}&units=imperial';
+    var weatherapiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
     fetch(weatherapiURL)
     .then(response =>response.json())
@@ -42,9 +51,10 @@ function getWeather(latitude, longitude) {
 }
 
 function displayCurrentWeather(weatherData) {
+    console.log(weatherData);
     var weatherCard = document.getElementById('current-weather-card')
-    weatherCard.querySelector('.card-title').textContent = 'Weather in ${weatherData.name}';
-    weatherCard.querySelector('.card-text').textContent = 'Temperature: ${weatherData.main.temp}°F';
+    weatherCard.querySelector('.card-title').textContent = `Weather in ${weatherData.name}`;
+    weatherCard.querySelector('.card-text').textContent = `Temperature: ${weatherData.main.temp}°F`;
 }
 /*var getWeather = function (CityName) {
     var apiURL = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key};
@@ -63,3 +73,23 @@ function displayCurrentWeather(weatherData) {
             alert('Unable to get weather data');
         });
 */
+
+function loadSearchHistory() {
+    let history = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    let historyContainer = document.getElementById('search-history'); // Make sure this ID exists in your HTML
+    historyContainer.innerHTML = ''; // Clear existing content
+
+    history.forEach(city => {
+        let cityElement = document.createElement('button');
+        cityElement.textContent = city;
+        cityElement.classList.add('list-group-item', 'list-group-item-action');
+        cityElement.onclick = function() {
+            getGeoLocation(city); // Fetch weather data for the clicked city
+        };
+        historyContainer.appendChild(cityElement);
+    });
+window.onload = function() {
+    loadSearchHistory();
+ };
+}
+
